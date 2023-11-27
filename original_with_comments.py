25fd4d7c9e0139b503117b89b3eab46e0fd7ad09
wianwer <118386460+wianwer@users.noreply.github.com>
2023-11-27 16:06:23 -0500

    original code with comments

--- original_with_comments.py
+++ original_with_comments.py
@@ -1,26 +1,13 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Dec  3 15:31:15 2022
+# Importing essential libraries
+import pygame  # Pygame facilitates the import of all necessary modules for game development
+import time  # The time module represents time in code using objects, numbers, and strings, offering various time-related functionalities
+import random  # The random module is employed for generating and manipulating random integers
 
-@author: sharonmoorthy
-"""
-# SM - Sharon Moorthy
-# BL - Bhawna Lachman
-# NY - Nitya Yadav
-# WA - Wardah Anwer
+pygame.init()  # Initializing all the imported pygame modules
 
-
-# WA -  importing libraries
-import pygame #WA - allows to import all the available pygame modules necessary into the package
-import time #WA - this module represents time in code within objects, numbers, and strings; provides functionality
-import random #WA - manipulates and generates random integers
-
-pygame.init() # WA - initializing all imported pygame modules
-
-# WA - defining colors 
-# WA - each line of code, known has RGB (red, green, and blue) can be combined in various proportions to obtain any color in the visible spectrum
-# WA - it additionally describes a colour as a tuple within the range of 0 and 255 that can be used on computer display
+# Defining a palette of colors
+# RGB (Red, Green, Blue) values in each line allow for a blend to produce a wide array of colors in the visible spectrum
+# These color codes, represented as tuples ranging from 0 to 255, are used for computer display
 white = (255, 255, 255)
 yellow = (255, 255, 102)
 black = (0, 0, 0)
@@ -28,191 +15,178 @@ red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
 
-# WA - screen size 
-screen_width = 600 # WA - width dimension 
-screen_height = 400 # WA - height dimension
+# Setting the screen dimensions
+screen_width = 600  # Width dimension
+screen_height = 400  # Height dimension
 
-# WA - initialising game window using the variables
+# Initializing the game window with the specified screen size variables
 screen = pygame.display.set_mode((screen_width, screen_height))
-# WA - when the screen appears, it will be automatically labelled by 'Snake Game'
-pygame.display.set_caption('Snake Game') 
+# The screen is automatically labeled as 'Snake Game' upon appearance
+pygame.display.set_caption('Snake Game')
 
-# WA - FPS (frames per second) controller 
-clock = pygame.time.Clock() #WA - it initially creates an object to track time and is represented in milliseconds
+# Controlling frames per second (FPS)
+clock = pygame.time.Clock()  # Creating a time-tracking object, measured in milliseconds
 
-# WA - frames of speed and movement; FPS can assist in controlling how fast or slow the "animation" must move 
+# Configuring frames for speed and movement; FPS regulates the animation's pace
 snake_block = 10
 snake_speed = 15
 
-# WA - creating font objects by stating the font style and size
+# Creating font objects with designated font style and size
 font_style = pygame.font.SysFont("bahnschrift", 25)
 score_font = pygame.font.SysFont("comicsansms", 35)
  
- 
-# BL - function for the user the score of what they are at
+# Function to visually represent the player's current score on the screen
 def game_score(score):  
-     # BL - value is the way the score will be presented on the screen in terms of the colour and words
-     value = score_font.render("Score: " + str(score), True, blue)  
-     # BL - displays the score on the screen at a certain position
-     screen.blit(value, [0, 0]) 
+     # Render the player's score using a specific font, color, and value
+     rendered_score = score_font.render("Score: " + str(score), True, blue)  
+     # Display the rendered score at a designated position on the screen
+     screen.blit(rendered_score, [0, 0]) 
  
  
- 
-# BL - function for the snake to grow each time it eats the apple
+# Function responsible for illustrating the snake's growth upon consuming the apple
 def snake_growth(snake_block, snake_list): 
-     # BL - for each block in the snake's body
-     for x in snake_list: 
-         # BL - pygame.draw.rect creates a rectangle on the screen in a white colour at the end of the previous block
-         pygame.draw.rect(screen, white, [x[0], x[1], snake_block, snake_block]) 
+     # Iterate through each block in the snake's body
+     for block_position in snake_list: 
+         # Draw a white rectangle on the screen, representing each block in the snake
+         pygame.draw.rect(screen, white, [block_position[0], block_position[1], snake_block, snake_block]) 
  
- 
-# BL - function for let the user know when they lose
+# Function to visually convey a message on the screen
 def message(text, color): 
-     # BL - this is for the way the message will be presented on the screen
-     msg_type = font_style.render(text, True, color) 
-     # BL - displays the message on the screen at a certain position
-     screen.blit(msg_type, [screen_width / 6, screen_height / 3]) 
+     # Render the message using a specified font, style, and color
+     rendered_message = font_style.render(text, True, color) 
+     # Display the rendered message at a defined position on the screen
+     screen.blit(rendered_message, [screen_width / 6, screen_height / 3]) 
  
- 
-# BL - function for the game to continue or stop under certain conditions
+# Function governing the main game loop, determining when the game continues or stops
 def game_loop():
-     # BL - these are variables for when the game is running
+     # Variable indicating whether the game is currently over
      game_over = False 
-     # BL - conditions for when the game is running
+     # Variable handling specific conditions during active gameplay
      close_game = False 
      
-     # BL - this is where the snake will start in terms of the horizontal direction
+     # Initial horizontal position of the snake's head
      x1 = screen_width / 2 
-     # BL - this is the same but in the vertical direction
+     # Initial vertical position of the snake's head
      y1 = screen_height / 2 
      
-     # BL - these are variables where the position changes depending on the keys the user chooses, this is in the horizontal direction, at the start it is zero
+     # Variables representing the snake's movement in the horizontal and vertical directions
      x1_movement = 0 
-     # BL - this is new position of the snake in the vertical direction, at the start it is zero
      y1_movement = 0 
      
-     # BL - at the start of the game, the list will be empty since there is no growth yet
+     # List storing the positions of each block in the snake's body
      snake_List = [] 
-     # BL - at the beginning of the game, the snake starts with one block
+     # Initial length of the snake (starting with one block)
      snake_length = 1 
      
-     # BL - this is the random position that the computer generates for the x-direction for the apple to be placed
+     # Randomly generated position for the apple along the x-axis
      x_food = round(random.randrange(0, screen_width - snake_block) / 10.0) * 10.0 
-     # BL - this is the y-direction for the apple's location, both combined will produce a coordinate for the apple's position
+     # Randomly generated position for the apple along the y-axis
      y_food = round(random.randrange(0, screen_height - snake_block) / 10.0) * 10.0 
- 
-     while not game_over:
- 
-         #NY-the game is still not over.
-         while close_game == True: 
-             #NY-filling the screen with black colour.
-             screen.fill(black) 
-             #NY-prints "You Lost! Press C-Play Again or Q-Quit" message with blue ink.
-             message("You Lost! Press C-Play Again or Q-Quit", blue)
-             #NY-1 subtracted from the length is the new score now
-             game_score(snake_length - 1)
-             #NY-display is updated according to the score.
-             pygame.display.update()
 
-             #NY-loop for the functions executed by pressing the listed keys by the user
-             for event in pygame.event.get(): 
-                 #NY-if the user taps the down key
-                 if event.type == pygame.KEYDOWN:
-                     #NY-then tap q key
-                     if event.key == pygame.K_q: 
-                         #NY-game gets over
-                         game_over = True 
-                         close_game = False
-                    
-                     #NY-if the user taps c key then the game keeps on playing
-                     if event.key == pygame.K_c: 
-                         game_loop()
- 
-         #NY-loop for the functions executed by pressing the listed keys by the user                 
-         for event in pygame.event.get(): 
-           #NY-if the user taps QUIT then the game is over
-           if event.type == pygame.QUIT: 
-               game_over = True  
-           #NY-if down key is pressed
-           if event.type == pygame.KEYDOWN: 
-               #NY-the user taps on LEFT
-               if event.key == pygame.K_LEFT: 
-                   #NY-the snake moves towards left (a negative sign is used as the snake goes in the negative(left) direction)
-                   x1_movement = -snake_block 
-                   #NY-variable y1 is 0 as the snake doesn't move vertically
-                   y1_movement = 0 
-               #NY-the user taps on RIGHT
-               elif event.key == pygame.K_RIGHT: 
-                   #NY-the snake moves towards right
-                   x1_movement = snake_block 
-                   #NY-variable y1 is 0 as the snake doesn't move vertically
-                   y1_movement = 0 
-               #NY-the user taps on UP
-               elif event.key == pygame.K_UP: 
-                   #NY-the snake moves upwards (a negative sign is used as the snake goes in the negative(up) direction)
-                   y1_movement = -snake_block 
-                   #NY-variable x1 is 0 as the snake doesn't move horizontally
-                   x1_movement = 0 
-               #NY-the user taps on DOWN
-               elif event.key == pygame.K_DOWN: 
-                   #NY-the snake moves downwards
-                   y1_movement = snake_block 
-                   #NY-variable x1 is 0 as the snake doesn't move horizontally
-                   x1_movement = 0 
-        
- 
-         # SM - if the snake touches the border: 
-         if x1 >= screen_width or x1 < 0 or y1 >= screen_height or y1 < 0:
-          # SM - game over
-          close_game = True 
-          
-         # SM - movement of the snake in the x direction     
-         x1 += x1_movement 
-         # SM - movement of the snake in the y direction 
-         y1 += y1_movement 
-         # SM - color of the background screen
-         screen.fill(black) 
-         # S.M. - draw a rectangle [location, color, [x_coordinate, y_coordinate, width, height]]
-         pygame.draw.rect(screen, red, [x_food, y_food, snake_block, snake_block]) 
-         # SM - list containing the added snake heads that appear each time the snake eats food
-         snake_head = [] 
-         # SM - add the new snake head to the list causing the snake to grow by one block
-         snake_head.append(x1) 
-         snake_head.append(y1)
-         # SM - add the list snake_head to the snake_List so all the snake blocks are in the snake_List
-         snake_List.append(snake_head) 
-         # SM - if the length of the snake_List is greater than the snake length, delete the first item in the list
-         if len(snake_List) > snake_length: 
-             del snake_List[0]
- 
-         # SM - for variable x in every value of the list, up until the last one,
-         for x in snake_List[:-1]:
-            # SM - if the snake collides with itself, the game ends
-            if x == snake_head:
-                close_game = True
-                
-         # SM - calling the snake_growth function
-         snake_growth(snake_block, snake_List)
-         # SM - calling the game_score function, the score is the value of the snake length minus 1, since the snake already starts with one block
-         game_score(snake_length - 1)
-         # SM - making changes to the display screen
-         pygame.display.update()
- 
-         if x1 == x_food and y1 == y_food: 
-            # SM - randomly generate a new x_coordinate for the food (move the location)
-            x_food = round(random.randrange(0, screen_width - snake_block) / 10.0) * 10.0 
-            # SM - randomly generate a new y_coordinate
-            y_food = round(random.randrange(0, screen_height - snake_block) / 10.0) * 10.0
-            # SM - when the snake eats the food, increase the length of the snake by one
-            snake_length += 1 
- 
-         # SM - pygame function for runtim speed of the game, snake_speed will be the number of frames that pass per second 
-         clock.tick(snake_speed)
- 
-     # SM - closes pygame to ensure that it's not running after the while loop and after the game is over
-     pygame.quit()
-     # SM - built-in python command that closes the running program
-     quit()
- 
-#SM - calling the game_loop function to run the game 
+# Continue processing as long as the game is still in progress
+while not game_over:
+    # Execute this block when the game-over condition is true
+    while close_game == True: 
+        # Fill the screen with a black color to provide a clean slate
+        screen.fill(black) 
+        # Display a message informing the player of their loss and prompting them to press 'C' to play again or 'Q' to quit
+        message("You Lost! Press C-Play Again or Q-Quit", blue)
+        # Update the score display, subtracting 1 from the current snake length
+        game_score(snake_length - 1)
+        # Refresh the display to reflect the latest changes
+        pygame.display.update()
+
+        # Handle key events to respond to user input during the game-over state
+        for event in pygame.event.get(): 
+            # Check if a key has been pressed
+            if event.type == pygame.KEYDOWN:
+                # If the pressed key is 'Q', terminate the game
+                if event.key == pygame.K_q: 
+                    game_over = True 
+                    close_game = False
+                # If the pressed key is 'C', restart the game
+                if event.key == pygame.K_c: 
+                    game_loop()
+
+    # Handle key events during the main game loop                
+    for event in pygame.event.get(): 
+        # If the user closes the game window, end the game
+        if event.type == pygame.QUIT: 
+            game_over = True  
+        # If a key is pressed
+        if event.type == pygame.KEYDOWN: 
+            # If the left arrow key is pressed
+            if event.key == pygame.K_LEFT: 
+                # Move the snake to the left (negative direction) and maintain its vertical position
+                x1_movement = -snake_block 
+                y1_movement = 0 
+            # If the right arrow key is pressed
+            elif event.key == pygame.K_RIGHT: 
+                # Move the snake to the right and maintain its vertical position
+                x1_movement = snake_block 
+                y1_movement = 0 
+            # If the up arrow key is pressed
+            elif event.key == pygame.K_UP: 
+                # Move the snake upwards (negative direction) and maintain its horizontal position
+                y1_movement = -snake_block 
+                x1_movement = 0 
+            # If the down arrow key is pressed
+            elif event.key == pygame.K_DOWN: 
+                # Move the snake downwards and maintain its horizontal position
+                y1_movement = snake_block 
+                x1_movement = 0 
+
+if x1 >= screen_width or x1 < 0 or y1 >= screen_height or y1 < 0:
+    # Set the flag to close the game when the snake hits the border
+    close_game = True 
+
+# Update the snake's position in the x direction    
+x1 += x1_movement 
+# Update the snake's position in the y direction 
+y1 += y1_movement 
+# Fill the screen with a black color
+screen.fill(black) 
+# Draw a rectangle to represent the food [location, color, [x_coordinate, y_coordinate, width, height]]
+pygame.draw.rect(screen, red, [x_food, y_food, snake_block, snake_block]) 
+# Create a new snake head
+snake_head = [] 
+# Add the new snake head's coordinates to the list
+snake_head.append(x1) 
+snake_head.append(y1)
+# Add the new snake head to the snake_List for growth
+snake_List.append(snake_head) 
+# If the snake_List is longer than the snake's length, remove the oldest block
+if len(snake_List) > snake_length: 
+    del snake_List[0]
+
+# Check for collisions with the snake's body
+for x in snake_List[:-1]:
+    # If the snake collides with itself, set the flag to close the game
+    if x == snake_head:
+        close_game = True
+
+# Execute the snake growth function
+snake_growth(snake_block, snake_List)
+# Display the current score on the screen
+game_score(snake_length - 1)
+# Update the display screen
+pygame.display.update()
+
+# Check if the snake's head coordinates match the food coordinates
+if x1 == x_food and y1 == y_food: 
+    # Generate new random coordinates for the food
+    x_food = round(random.randrange(0, screen_width - snake_block) / 10.0) * 10.0 
+    y_food = round(random.randrange(0, screen_height - snake_block) / 10.0) * 10.0
+    # Increase the snake's length when it eats the food
+    snake_length += 1 
+
+# Control the game's speed by setting the number of frames per second
+clock.tick(snake_speed)
+
+# Close Pygame to stop running after the game ends
+pygame.quit()
+# Terminate the program
+quit()
+
+# Call the game_loop function to start or restart the game
 game_loop()
